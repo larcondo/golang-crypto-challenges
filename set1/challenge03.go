587f87e3f5b3f38f57a1b11c86463e1e43e180e8
larcondo <larcondo90@gmail.com>
2025-03-04 18:46:13 -0300

    improve solution to challenge 3 (set 1)

--- set1/challenge03.go
+++ set1/challenge03.go
@@ -1,12 +1,56 @@
 package set1_challenges
 
+import (
+	"fmt"
+	"sort"
+)
+
 // challenge 3 - Single-byte XOR cipher
-func singleByteXORDecypher(encrypted []byte, key byte) string {
+func singleByteXORDecypher(cipherText []byte, key byte) string {
 	var result []byte
 
-	for i := range encrypted {
-		result = append(result, encrypted[i]^key)
+	for i := range cipherText {
+		result = append(result, cipherText[i]^key)
 	}
 
 	return string(result)
+}
+
+func rankByteAppearance(cipherText []byte) []byte {
+	list := make(map[byte]int)
+
+	// create list of character appareance
+	for i := range cipherText {
+		list[cipherText[i]] += 1
+	}
+
+	// sort
+	keys := make([]byte, 0, len(list))
+	for key := range list {
+		keys = append(keys, key)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return list[keys[i]] > list[keys[j]]
+	})
+
+	return keys
+}
+
+func DecypherText(cipherText []byte) string {
+	// probable := []byte("ETAOIN SHRDLU")
+	probable := []byte("etaoin shrdlu")
+
+	keys := rankByteAppearance(cipherText)
+
+	for i := range probable {
+		// keys[1] se repite 2 veces en los primeros valores
+		clave := keys[0] ^ probable[i]
+		output := singleByteXORDecypher(cipherText, clave)
+		if isValidMessage(output) {
+			fmt.Println("Letra:", string(probable[i]), "Clave:", clave, "(", string(clave), ")", " -> Mensaje:", output)
+			return output
+		}
+	}
+
+	return ""
 }
